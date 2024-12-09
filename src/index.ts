9eeeaf321eb29534c81fdf7c421919e183e047a2
coderipper <hello@joaquinsoza.dev>
2024-12-09 17:32:32 -0300

    exporting @stellar/stellar-sdk

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 // Config
-export * from "./config/loader";
-export * from "./config/defaultNetworks";
-export * from "./config/toolkit";
+export * from './config/loader';
+export * from './config/defaultNetworks';
+export * from './config/toolkit';
 
 // Utils
-export * from "./utils/accountUtils";
+export * from './utils/accountUtils';
 
 // Managers
-export * from "./managers/contract";
-export * from "./managers/transaction";
-export * from "./managers/token";
+export * from './managers/contract';
+export * from './managers/transaction';
+export * from './managers/token';
+
+// Export * from stellar-sdk
+export * from '@stellar/stellar-sdk';
